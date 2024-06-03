e90587c50b7823f10d9db7317eaaf6684f20c085
auskes <132512097+auskes@users.noreply.github.com>
2024-06-02 18:33:08 -0700

    hidevacant patch

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -415,9 +415,15 @@ buttonpress(XEvent *e)
 	}
         if (ev->window == selmon->barwin) {
                 i = x = 0;
-                do
+                unsigned int occ = 0;
+		for(c = m->clients; c; c=c->next)
+			occ |= c->tags == TAGMASK ? 0 : c->tags;
+		do {
+			/* Do not reserve space for vacant tags */
+			if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+				continue;
                         x += TEXTW(tags[i]);
-                while (ev->x >= x && ++i < LENGTH(tags));
+                } while (ev->x >= x && ++i < LENGTH(tags));
                 if (i < LENGTH(tags)) {
                 } else if (ev->x < x + TEXTW(selmon->ltsymbol)) {
                  } else {
@@ -677,12 +683,15 @@ drawbar(Monitor *m)
     }
 
     for (c = m->clients; c; c = c->next) {
-        occ |= c->tags;
+        occ |= c->tags == TAGMASK ? 0 : c->tags;
         if (c->isurgent)
             urg |= c->tags;
     }
     x = 0;
     for (i = 0; i < LENGTH(tags); i++) {
+	/* Do not draw vacant tags */
+	if(!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+		continue;
         w = TEXTW(tags[i]);
         if (urg & 1 << i) {
             drw_setscheme(drw, scheme[SchemeUrg]);
